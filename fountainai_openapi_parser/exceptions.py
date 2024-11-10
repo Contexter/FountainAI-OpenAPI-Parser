d2016037fbb1e1d10fa49c949f5263ba1c80b855
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2024-11-10 10:52:10 +0100

    docs: Add detailed attribute descriptions to exceptions.py
    - Updated docstrings to include attribute descriptions for better documentation.
    - Clarified the purpose and usage of each exception attribute.

--- fountainai_openapi_parser/exceptions.py
+++ fountainai_openapi_parser/exceptions.py
@@ -1,6 +1,9 @@
 class ParsingError(Exception):
     """
     Raised when an error occurs during parsing of the OpenAPI document.
+
+    Attributes:
+        message (str): Description of the parsing error.
     """
     def __init__(self, message: str):
         super().__init__(message)
@@ -10,6 +13,9 @@ class ParsingError(Exception):
 class ValidationError(Exception):
     """
     Raised when the OpenAPI document fails validation.
+
+    Attributes:
+        message (str): Description of the validation error.
     """
     def __init__(self, message: str):
         super().__init__(message)
@@ -19,8 +25,10 @@ class ValidationError(Exception):
 class ReferenceResolutionError(Exception):
     """
     Raised when there is an issue resolving $ref references in the OpenAPI document.
+
+    Attributes:
+        message (str): Description of the reference resolution error.
     """
     def __init__(self, message: str):
         super().__init__(message)
         self.message = message
-
