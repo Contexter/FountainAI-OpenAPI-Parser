800a6048810825dca9644e251656e08ac76def70
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2024-11-12 11:48:48 +0100

    Update parameterlocation.py

--- fountainai_openapi_parser/models/parameterlocation.py
+++ fountainai_openapi_parser/models/parameterlocation.py
@@ -1,11 +1,9 @@
-from typing import Dict, Union from pydantic import RootModel
+from enum import Enum
+from pydantic import RootModel
+
 
 class ParameterLocation(str, Enum):
-
-QUERY = "query"
-
-HEADER = "header"
-
-PATH = "path"
-
-COOKIE = "cookie"
+    QUERY = "query"
+    HEADER = "header"
+    PATH = "path"
+    COOKIE = "cookie"
