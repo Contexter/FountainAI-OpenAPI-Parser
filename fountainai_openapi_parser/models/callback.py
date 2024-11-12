2b577e0d4445742abdb942567f74e860fcd1d66e
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2024-11-12 11:29:08 +0100

    Update callback.py

--- fountainai_openapi_parser/models/callback.py
+++ fountainai_openapi_parser/models/callback.py
@@ -1,11 +1,9 @@
-from fountainai_openapi_parser.models import PathItem
-
-from fountainai_openapi_parser.models import Reference
-
 from typing import Dict, Union
-
 from pydantic import RootModel
 
-class Callback(RootModel[Dict[str, Union["PathItem", Reference]]]):
+from fountainai_openapi_parser.models import PathItem
+from fountainai_openapi_parser.models import Reference
 
-pass
+
+class Callback(RootModel[Dict[str, Union["PathItem", Reference]]]):
+    pass
