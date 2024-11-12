21127b9538338f0fff718f8d2382b6865347b20f
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2024-11-12 11:41:31 +0100

    Update link.py

--- fountainai_openapi_parser/models/link.py
+++ fountainai_openapi_parser/models/link.py
@@ -1,15 +1,12 @@
-from typing import Dict, Union from pydantic import RootModel
+from typing import Dict, Optional, Any
+from pydantic import BaseModel
+from fountainai_openapi_parser.models import Server
+
 
 class Link(BaseModel):
-
-operationRef: Optional[str] = None
-
-operationId: Optional[str] = None
-
-parameters: Optional[Dict[str, Any]] = None
-
-requestBody: Optional[Any] = None
-
-description: Optional[str] = None
-
-server: Optional[Server] = None
+    operationRef: Optional[str] = None
+    operationId: Optional[str] = None
+    parameters: Optional[Dict[str, Any]] = None
+    requestBody: Optional[Any] = None
+    description: Optional[str] = None
+    server: Optional[Server] = None
