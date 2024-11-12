8210bc59fd2c6bd7f2c5e4561841815f0678b2ef
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2024-11-12 11:31:04 +0100

    Update contact.py

--- fountainai_openapi_parser/models/contact.py
+++ fountainai_openapi_parser/models/contact.py
@@ -1,9 +1,8 @@
-from typing import Dict, Union from pydantic import RootModel
+from typing import Optional
+from pydantic import BaseModel, AnyUrl, EmailStr
+
 
 class Contact(BaseModel):
-
-name: Optional[str] = None
-
-url: Optional[AnyUrl] = None
-
-email: Optional[EmailStr] = None
+    name: Optional[str] = None
+    url: Optional[AnyUrl] = None
+    email: Optional[EmailStr] = None
