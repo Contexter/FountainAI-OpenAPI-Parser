00b8ebf77ecc4b8f41eb2aec3f5a00cc25bc34a3
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2024-11-12 11:49:32 +0100

    Update response.py

--- fountainai_openapi_parser/models/response.py
+++ fountainai_openapi_parser/models/response.py
@@ -1,13 +1,10 @@
-from fountainai_openapi_parser.models import Reference
+from typing import Dict, Union, Optional
+from pydantic import BaseModel
+from fountainai_openapi_parser.models import Reference, Header, MediaType, Link
 
-from typing import Dict, Union from pydantic import RootModel
 
 class Response(BaseModel):
-
-description: str
-
-headers: Optional[Dict[str, Union["Header", Reference]]] = None
-
-content: Optional[Dict[str, MediaType]] = None
-
-links: Optional[Dict[str, Union["Link", Reference]]] = None
+    description: str
+    headers: Optional[Dict[str, Union[Header, Reference]]] = None
+    content: Optional[Dict[str, MediaType]] = None
+    links: Optional[Dict[str, Union[Link, Reference]]] = None
