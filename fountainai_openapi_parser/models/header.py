d15e6c9894990ea91146b0b37c57938515d6da84
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2024-11-12 11:36:20 +0100

    Update header.py

--- fountainai_openapi_parser/models/header.py
+++ fountainai_openapi_parser/models/header.py
@@ -1,27 +1,17 @@
-from fountainai_openapi_parser.models import Reference
+from typing import Dict, Union, Optional, Any
+from pydantic import BaseModel
+from fountainai_openapi_parser.models import Reference, Style, Schema, Example, MediaType
 
-from typing import Dict, Union from pydantic import RootModel
 
 class Header(BaseModel):
-
-description: Optional[str] = None
-
-required: Optional[bool] = None
-
-deprecated: Optional[bool] = None
-
-allowEmptyValue: Optional[bool] = None
-
-style: Optional[Style] = None
-
-explode: Optional[bool] = None
-
-allowReserved: Optional[bool] = None
-
-schema_data: Optional[Union[Schema, Reference]] = None
-
-example: Optional[Any] = None
-
-examples: Optional[Dict[str, Union[Example, Reference]]] = None
-
-content: Optional[Dict[str, MediaType]] = None
+    description: Optional[str] = None
+    required: Optional[bool] = None
+    deprecated: Optional[bool] = None
+    allowEmptyValue: Optional[bool] = None
+    style: Optional[Style] = None
+    explode: Optional[bool] = None
+    allowReserved: Optional[bool] = None
+    schema_data: Optional[Union[Schema, Reference]] = None
+    example: Optional[Any] = None
+    examples: Optional[Dict[str, Union[Example, Reference]]] = None
+    content: Optional[Dict[str, MediaType]] = None
