eede33c1e430556bbb8b9ca07ec71b6e3c71f740
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2024-11-12 11:52:54 +0100

    Update reference.py

--- fountainai_openapi_parser/models/reference.py
+++ fountainai_openapi_parser/models/reference.py
@@ -1,16 +1,11 @@
-from fountainai_openapi_parser.models import Reference
+from typing import Optional
+from pydantic import BaseModel, Field
 
-from typing import Dict, Union from pydantic import RootModel
 
 class Reference(BaseModel):
+    ref: str = Field(..., alias="$ref")
+    summary: Optional[str] = None
+    description: Optional[str] = None
 
-ref: str = Field(..., alias="$ref")
-
-summary: Optional[str] = None
-
-description: Optional[str] = None
-
-
-class Config:
-
-populate_by_name = True
+    class Config:
+        populate_by_name = True
