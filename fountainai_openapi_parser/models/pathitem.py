2756b591b46ebe7d42d904b41416d5f6ebc5c5f2
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2024-11-12 11:51:01 +0100

    Update pathitem.py

--- fountainai_openapi_parser/models/pathitem.py
+++ fountainai_openapi_parser/models/pathitem.py
@@ -1,38 +1,22 @@
-from fountainai_openapi_parser.models import PathItem
+from typing import Dict, Optional, Union, List
+from pydantic import BaseModel, Field
+from fountainai_openapi_parser.models import Operation, Server, Parameter, Reference
 
-from fountainai_openapi_parser.models import Reference
-
-from typing import Dict, Union from pydantic import RootModel
 
 class PathItem(BaseModel):
+    ref: Optional[str] = Field(default=None, alias="$ref")
+    summary: Optional[str] = None
+    description: Optional[str] = None
+    get: Optional[Operation] = None
+    put: Optional[Operation] = None
+    post: Optional[Operation] = None
+    delete: Optional[Operation] = None
+    options: Optional[Operation] = None
+    head: Optional[Operation] = None
+    patch: Optional[Operation] = None
+    trace: Optional[Operation] = None
+    servers: Optional[List[Server]] = None
+    parameters: Optional[List[Union[Parameter, Reference]]] = None
 
-ref: Optional[str] = Field(default=None, alias="$ref")
-
-summary: Optional[str] = None
-
-description: Optional[str] = None
-
-get: Optional[Operation] = None
-
-put: Optional[Operation] = None
-
-post: Optional[Operation] = None
-
-delete: Optional[Operation] = None
-
-options: Optional[Operation] = None
-
-head: Optional[Operation] = None
-
-patch: Optional[Operation] = None
-
-trace: Optional[Operation] = None
-
-servers: Optional[List[Server]] = None
-
-parameters: Optional[List[Union[Parameter, Reference]]] = None
-
-
-class Config:
-
-populate_by_name = True
+    class Config:
+        populate_by_name = True
