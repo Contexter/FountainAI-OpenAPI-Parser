bb4d2df1e3670011f8c325fb67a94e9cf30165d8
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2024-11-12 11:57:51 +0100

    Update style.py

--- fountainai_openapi_parser/models/style.py
+++ fountainai_openapi_parser/models/style.py
@@ -1,17 +1,11 @@
-from typing import Dict, Union from pydantic import RootModel
+from enum import Enum
+
 
 class Style(str, Enum):
-
-MATRIX = "matrix"
-
-LABEL = "label"
-
-FORM = "form"
-
-SIMPLE = "simple"
-
-SPACE_DELIMITED = "spaceDelimited"
-
-PIPE_DELIMITED = "pipeDelimited"
-
-DEEP_OBJECT = "deepObject"
+    MATRIX = "matrix"
+    LABEL = "label"
+    FORM = "form"
+    SIMPLE = "simple"
+    SPACE_DELIMITED = "spaceDelimited"
+    PIPE_DELIMITED = "pipeDelimited"
+    DEEP_OBJECT = "deepObject"
