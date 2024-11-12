ed537a242b693aa16b454c60fafeb43104f6de32
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2024-11-12 11:42:31 +0100

    Update mediatype.py

--- fountainai_openapi_parser/models/mediatype.py
+++ fountainai_openapi_parser/models/mediatype.py
@@ -1,13 +1,10 @@
-from fountainai_openapi_parser.models import Reference
+from typing import Dict, Union, Optional, Any
+from pydantic import BaseModel
+from fountainai_openapi_parser.models import Reference, Example, Encoding, Schema
 
-from typing import Dict, Union from pydantic import RootModel
 
 class MediaType(BaseModel):
-
-schema_data: Optional[Union["Schema", Reference]] = None
-
-example: Optional[Any] = None
-
-examples: Optional[Dict[str, Union[Example, Reference]]] = None
-
-encoding: Optional[Dict[str, Encoding]] = None
+    schema_data: Optional[Union[Schema, Reference]] = None
+    example: Optional[Any] = None
+    examples: Optional[Dict[str, Union[Example, Reference]]] = None
+    encoding: Optional[Dict[str, Encoding]] = None
