2091618809ae2b1c980a16fa2568730251686e68
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2024-11-12 12:00:31 +0100

    Update server.py

--- fountainai_openapi_parser/models/server.py
+++ fountainai_openapi_parser/models/server.py
@@ -1,9 +1,9 @@
-from typing import Dict, Union from pydantic import RootModel
+from typing import Dict, Optional
+from pydantic import BaseModel
+from fountainai_openapi_parser.models.servervariable import ServerVariable
+
 
 class Server(BaseModel):
-
-url: str
-
-description: Optional[str] = None
-
-variables: Optional[Dict[str, ServerVariable]] = None
+    url: str
+    description: Optional[str] = None
+    variables: Optional[Dict[str, ServerVariable]] = None
