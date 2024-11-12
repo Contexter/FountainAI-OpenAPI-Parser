fcae6a744cd1c502138d94b6479007c904c208e2
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2024-11-12 11:47:53 +0100

    Update requestbody.py

--- fountainai_openapi_parser/models/requestbody.py
+++ fountainai_openapi_parser/models/requestbody.py
@@ -1,9 +1,9 @@
-from typing import Dict, Union from pydantic import RootModel
+from typing import Dict, Optional
+from pydantic import BaseModel
+from fountainai_openapi_parser.models.mediatype import MediaType
+
 
 class RequestBody(BaseModel):
-
-description: Optional[str] = None
-
-content: Dict[str, MediaType]
-
-required: Optional[bool] = None
+    description: Optional[str] = None
+    content: Dict[str, MediaType]
+    required: Optional[bool] = None
