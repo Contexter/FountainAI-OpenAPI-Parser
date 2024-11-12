6c488b6321ece01840c5a9442bdf0fd04217a0c7
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2024-11-12 11:56:53 +0100

    Update securityscheme.py

--- fountainai_openapi_parser/models/securityscheme.py
+++ fountainai_openapi_parser/models/securityscheme.py
@@ -1,24 +1,17 @@
-from typing import Dict, Union from pydantic import RootModel
+from typing import Optional
+from pydantic import BaseModel, Field, AnyUrl
+from fountainai_openapi_parser.models import SecuritySchemeType, ParameterLocation, OAuthFlows
+
 
 class SecurityScheme(BaseModel):
+    type: SecuritySchemeType
+    description: Optional[str] = None
+    name: Optional[str] = None
+    in_: Optional[ParameterLocation] = Field(default=None, alias="in")
+    scheme: Optional[str] = None
+    bearerFormat: Optional[str] = None
+    flows: Optional[OAuthFlows] = None
+    openIdConnectUrl: Optional[AnyUrl] = None
 
-type: SecuritySchemeType
-
-description: Optional[str] = None
-
-name: Optional[str] = None
-
-in_: Optional[ParameterLocation] = Field(default=None, alias="in")
-
-scheme: Optional[str] = None
-
-bearerFormat: Optional[str] = None
-
-flows: Optional["OAuthFlows"] = None
-
-openIdConnectUrl: Optional[AnyUrl] = None
-
-
-class Config:
-
-populate_by_name = True
+    class Config:
+        populate_by_name = True
