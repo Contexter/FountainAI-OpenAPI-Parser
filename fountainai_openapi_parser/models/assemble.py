89def1fb06f23357d61161aa59625b7e8e443368
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2024-11-12 11:28:02 +0100

    Update assemble.py

--- fountainai_openapi_parser/models/assemble.py
+++ fountainai_openapi_parser/models/assemble.py
@@ -1,70 +1,39 @@
-from typing import Dict, Union from pydantic import RootModel
+from typing import Dict, Union
+from pydantic import RootModel
 
 from . import *
 
 
 def get_models():
-
-return {
-
-'callback': callback,
-
-'link': link,
-
-'requestbody': requestbody,
-
-'server': server,
-
-'encoding': encoding,
-
-'style': style,
-
-'header': header,
-
-'externaldocumentation': externaldocumentation,
-
-'parameterlocation': parameterlocation,
-
-'openapi': openapi,
-
-'xml': xml,
-
-'response': response,
-
-'operation': operation,
-
-'oauthflow': oauthflow,
-
-'oauthflows': oauthflows,
-
-'example': example,
-
-'mediatype': mediatype,
-
-'securityschemetype': securityschemetype,
-
-'reference': reference,
-
-'license': license,
-
-'components': components,
-
-'info': info,
-
-'parameter': parameter,
-
-'securityscheme': securityscheme,
-
-'discriminator': discriminator,
-
-'servervariable': servervariable,
-
-'tag': tag,
-
-'pathitem': pathitem,
-
-'contact': contact,
-
-'schema': schema,
-
-}
+    return {
+        'callback': callback,
+        'link': link,
+        'requestbody': requestbody,
+        'server': server,
+        'encoding': encoding,
+        'style': style,
+        'header': header,
+        'externaldocumentation': externaldocumentation,
+        'parameterlocation': parameterlocation,
+        'openapi': openapi,
+        'xml': xml,
+        'response': response,
+        'operation': operation,
+        'oauthflow': oauthflow,
+        'oauthflows': oauthflows,
+        'example': example,
+        'mediatype': mediatype,
+        'securityschemetype': securityschemetype,
+        'reference': reference,
+        'license': license,
+        'components': components,
+        'info': info,
+        'parameter': parameter,
+        'securityscheme': securityscheme,
+        'discriminator': discriminator,
+        'servervariable': servervariable,
+        'tag': tag,
+        'pathitem': pathitem,
+        'contact': contact,
+        'schema': schema,
+    }
