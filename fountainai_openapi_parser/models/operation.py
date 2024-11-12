3494fe64a19b44c3e2884be5697aebd6429ae337
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2024-11-12 11:44:57 +0100

    Update operation.py

--- fountainai_openapi_parser/models/operation.py
+++ fountainai_openapi_parser/models/operation.py
@@ -1,29 +1,26 @@
-from fountainai_openapi_parser.models import Reference
+from typing import Dict, Union, Optional, List
+from pydantic import BaseModel
+from fountainai_openapi_parser.models import (
+    Reference,
+    ExternalDocumentation,
+    Parameter,
+    RequestBody,
+    Response,
+    Callback,
+    Server,
+)
 
-from typing import Dict, Union from pydantic import RootModel
 
 class Operation(BaseModel):
-
-tags: Optional[List[str]] = None
-
-summary: Optional[str] = None
-
-description: Optional[str] = None
-
-externalDocs: Optional[ExternalDocumentation] = None
-
-operationId: Optional[str] = None
-
-parameters: Optional[List[Union[Parameter, Reference]]] = None
-
-requestBody: Optional[Union[RequestBody, Reference]] = None
-
-responses: Dict[str, Union[Response, Reference]]
-
-callbacks: Optional[Dict[str, Union[Callback, Reference]]] = None
-
-deprecated: Optional[bool] = None
-
-security: Optional[List[Dict[str, List[str]]]] = None  # SecurityRequirements
-
-servers: Optional[List[Server]] = None
+    tags: Optional[List[str]] = None
+    summary: Optional[str] = None
+    description: Optional[str] = None
+    externalDocs: Optional[ExternalDocumentation] = None
+    operationId: Optional[str] = None
+    parameters: Optional[List[Union[Parameter, Reference]]] = None
+    requestBody: Optional[Union[RequestBody, Reference]] = None
+    responses: Dict[str, Union[Response, Reference]]
+    callbacks: Optional[Dict[str, Union[Callback, Reference]]] = None
+    deprecated: Optional[bool] = None
+    security: Optional[List[Dict[str, List[str]]]] = None  # SecurityRequirements
+    servers: Optional[List[Server]] = None
