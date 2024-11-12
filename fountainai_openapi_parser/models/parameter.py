671f2226ab23cf7cdef895ed12e94a2d0bc280a7
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2024-11-12 11:54:09 +0100

    Update parameter.py

--- fountainai_openapi_parser/models/parameter.py
+++ fountainai_openapi_parser/models/parameter.py
@@ -1,36 +1,22 @@
-from fountainai_openapi_parser.models import Reference
+from typing import Any, Dict, Optional, Union
+from pydantic import BaseModel, Field
+from fountainai_openapi_parser.models import ParameterLocation, Style, Schema, Reference, Example, MediaType
 
-from typing import Dict, Union from pydantic import RootModel
 
 class Parameter(BaseModel):
+    name: str
+    in_: ParameterLocation = Field(..., alias="in")
+    description: Optional[str] = None
+    required: Optional[bool] = None
+    deprecated: Optional[bool] = None
+    allowEmptyValue: Optional[bool] = None
+    style: Optional[Style] = None
+    explode: Optional[bool] = None
+    allowReserved: Optional[bool] = None
+    schema_data: Optional[Union[Schema, Reference]] = None
+    example: Optional[Any] = None
+    examples: Optional[Dict[str, Union[Example, Reference]]] = None
+    content: Optional[Dict[str, MediaType]] = None
 
-name: str
-
-in_: ParameterLocation = Field(..., alias="in")
-
-description: Optional[str] = None
-
-required: Optional[bool] = None
-
-deprecated: Optional[bool] = None
-
-allowEmptyValue: Optional[bool] = None
-
-style: Optional[Style] = None
-
-explode: Optional[bool] = None
-
-allowReserved: Optional[bool] = None
-
-schema_data: Optional[Union[Schema, Reference]] = None
-
-example: Optional[Any] = None
-
-examples: Optional[Dict[str, Union[Example, Reference]]] = None
-
-content: Optional[Dict[str, MediaType]] = None
-
-
-class Config:
-
-populate_by_name = True
+    class Config:
+        populate_by_name = True
