136131e8ba11b71491821eac01ed3c46b96ae3bc
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2024-11-12 11:46:57 +0100

    update oauth singl and plural

--- fountainai_openapi_parser/models/oauthflow.py
+++ fountainai_openapi_parser/models/oauthflow.py
@@ -1,11 +1,9 @@
-from typing import Dict, Union from pydantic import RootModel
+from typing import Dict, Optional
+from pydantic import BaseModel, AnyUrl
+
 
 class OAuthFlow(BaseModel):
-
-authorizationUrl: Optional[AnyUrl] = None
-
-tokenUrl: Optional[AnyUrl] = None
-
-refreshUrl: Optional[AnyUrl] = None
-
-scopes: Dict[str, str]
+    authorizationUrl: Optional[AnyUrl] = None
+    tokenUrl: Optional[AnyUrl] = None
+    refreshUrl: Optional[AnyUrl] = None
+    scopes: Dict[str, str]
--- fountainai_openapi_parser/models/oauthflows.py
+++ fountainai_openapi_parser/models/oauthflows.py
@@ -1,11 +1,10 @@
-from typing import Dict, Union from pydantic import RootModel
+from typing import Optional
+from pydantic import BaseModel
+from fountainai_openapi_parser.models.oauthflow import OAuthFlow
+
 
 class OAuthFlows(BaseModel):
-
-implicit: Optional["OAuthFlow"] = None
-
-password: Optional["OAuthFlow"] = None
-
-clientCredentials: Optional["OAuthFlow"] = None
-
-authorizationCode: Optional["OAuthFlow"] = None
+    implicit: Optional[OAuthFlow] = None
+    password: Optional[OAuthFlow] = None
+    clientCredentials: Optional[OAuthFlow] = None
+    authorizationCode: Optional[OAuthFlow] = None
