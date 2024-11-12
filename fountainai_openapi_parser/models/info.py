5344e7a49babbd0e1c480fc7351cf17feae1c5c4
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2024-11-12 11:37:16 +0100

    Update info.py

--- fountainai_openapi_parser/models/info.py
+++ fountainai_openapi_parser/models/info.py
@@ -1,15 +1,12 @@
-from typing import Dict, Union from pydantic import RootModel
+from typing import Optional
+from pydantic import BaseModel, AnyUrl
+from fountainai_openapi_parser.models import Contact, License
+
 
 class Info(BaseModel):
-
-title: str
-
-description: Optional[str] = None
-
-termsOfService: Optional[AnyUrl] = None
-
-contact: Optional[Contact] = None
-
-license: Optional[License] = None
-
-version: str
+    title: str
+    description: Optional[str] = None
+    termsOfService: Optional[AnyUrl] = None
+    contact: Optional[Contact] = None
+    license: Optional[License] = None
+    version: str
